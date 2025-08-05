7698a7c304246ea32525ed6a79af1f15b8f91f4d
Duskaryis <derekpayne1991@hotmail.com>
2025-08-05 16:31:11 -0400

    solved e4

--- exercises/e4.js
+++ exercises/e4.js
@@ -28,13 +28,7 @@ export const handlePromise = first()
     console.log(val);
     return val;
   });
-/* const secondPromise = handlePromise.then((val) => val);
-const final = secondPromise.then((res) => second(res));
-final.then((val) => {
-  console.log(val);
-  return val;
-});
- */
+
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-4"
 // If the test has all tests passed, switch to the next exercise file
