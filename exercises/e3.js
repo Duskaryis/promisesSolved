b4a945df1cb31fa99c2d617b69eb5fc79fdf1e35
Duskaryis <derekpayne1991@hotmail.com>
2025-08-05 16:09:15 -0400

    solved e3

--- exercises/e3.js
+++ exercises/e3.js
@@ -14,8 +14,13 @@
  * * getPromise(num) function must be exported
  * Example: export const getPromise(num) => return <Your code of promise>
  */
-export const getPromise = () => {
+export const getPromise = (num) => {
   // Your code goes here...
+  if (Number.isInteger(num)) {
+    return new Promise((resolve) => resolve(num));
+  } else {
+    return 0;
+  }
 };
 
 /**
@@ -31,6 +36,14 @@ export const getPromise = () => {
  */
 export const updateSumValue = () => {
   // Your code goes here...
+  let sum = 2;
+
+  getPromise(120).then((value) => {
+    sum += value;
+  });
+
+  sum += 8;
+  return sum;
 };
 
 // === TEST YOURSELF ===
